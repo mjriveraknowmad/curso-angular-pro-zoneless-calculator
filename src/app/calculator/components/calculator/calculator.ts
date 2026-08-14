b2d7e9c2de6ec322c0e3644a7297f5fad27a14de
Marcos Rivera <marcos.rivera@knowmadmood.com>
2026-08-14 14:21:55 +0200

    feat: Implementación de calculadora sin funcionalidad con tailwindcss, usand la propiedad host de @Component e InputSignals (con transforms) para comprobar desde la directiva [class] del html

---
+++ src/app/calculator/components/calculator/calculator.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CalculatorButton } from '../calculator-button/calculator-button';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [CalculatorButton],
+  templateUrl: './calculator.html',
+  host: {
+    class: 'max-w-96 overflow-hidden block',
+  },
+})
+export class Calculator {}
---
+++ src/app/calculator/views/calculator-view/calculator-view.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Calculator } from '../../components/calculator/calculator';
+
+@Component({
+  selector: 'calculator-view',
+  imports: [ Calculator ],
+  templateUrl: './calculator-view.html'
+})
+export default class CalculatorView {}
