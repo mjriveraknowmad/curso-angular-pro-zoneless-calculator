e8227109ba339a60123043aafed03fd80e13a28b
Marcos Rivera <marcos.rivera@knowmadmood.com>
2026-08-17 12:01:22 +0200

    feat: test de existencia de div con clases, href y otros

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,9 +14,33 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-    it('should render router-outlet', () => {
+  it('should render router-outlet', () => {
     const fixture = TestBed.createComponent(App);
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
+  it('should render router-outlet with classes', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const div = compiled.querySelector('div');
+    expect(div).toBeTruthy();
+    expect(div?.classList.contains('bg-slate-600')).toBe(true);
+  });
+
+  it('should render buy me a beer link', () => {
+    const fixture = TestBed.createComponent(App);
+    const compiled = fixture.nativeElement as HTMLElement;
+    const secondDivInFile = compiled.querySelector('div + div');
+    expect(secondDivInFile).toBeTruthy();
+    const divContained = secondDivInFile?.querySelector('div');
+    const link = divContained?.querySelector('a');
+    expect(link).toBeTruthy();
+    expect(link?.getAttribute('href')).toBe('https://www.buymeacoffee.com/scottwindon');
+    expect(link?.getAttribute('target')).toBe('_blank');
+    expect(link?.getAttribute('title')).toBe('Buy me a beer');
+    const img = link?.querySelector('img');
+    expect(img).toBeTruthy();
+    expect(img?.getAttribute('src')).toBe('https://i.pinimg.com/originals/60/fd/e8/60fde811b6be57094e0abc69d9c2622a.jpg');
+  });
+
 });
