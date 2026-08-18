b01d0d166ccd5d8ced652d948c55515d1b1db294
Marcos Rivera <marcos.rivera@knowmadmood.com>
2026-08-18 09:16:50 +0200

    feat: test inicial de CalculatorButton, comprobar si existe

---
+++ src/app/calculator/components/calculator-button/calculator-button.spec.ts
@@ -0,0 +1,50 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { CalculatorButton } from './calculator-button';
+
+// Mock del componente Calculator para evitar dependencias externas y centrarnos en las pruebas del componente CalculatorView.
+@Component({
+  selector: 'calculator',
+  template: '<div>Mock Calculator</div>',
+})
+class MockCalculatorComponent {}
+
+describe('CalculatorButton', () => {
+  let component: CalculatorButton;
+  let fixture: ComponentFixture<CalculatorButton>;
+
+  // No necesito que renderice el componente real de Calculator, así que creo un mock para él. Esto permite que las pruebas se centren en el componente CalculatorView sin depender de la implementación del componente Calculator.
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CalculatorButton],
+    });
+
+    fixture = TestBed.createComponent(CalculatorButton);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  // it('should render the calculator component', () => {
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   console.log(compiled.innerHTML);
+  //   expect(compiled.querySelector('calculator')).toBeTruthy();
+  // });
+
+  // it('should contain the specific CSS classes in the wrapper div', () => {
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   const divElement = compiled.querySelector('div');
+
+  //   const expectedClasses = 'clase-personalizada'.split(' ');
+
+  //   console.log('divElement classes:', divElement?.classList);
+
+  //   expectedClasses.forEach((className) => {
+  //     expect(divElement?.classList).toContain(className);
+  //   });
+  // });
+});
